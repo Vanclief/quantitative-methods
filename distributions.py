324f1c3f72572b8d467e0d9351144e5048649b47
Franco Valencia <franco.avalencia@gmail.com>
2018-02-05 12:28:17 -0600

    Added normal distribution

--- distributions.py
+++ distributions.py
@@ -2,12 +2,53 @@
 
 import os
 import sys
+import math
 
-def distrubution():
-    input("Ingresa el rango inferior de la sumatoria (i)")
+def distribution():
+    i = float(input("Ingresa el rango inferior de la sumatoria:"))
+    n = float(input("Ingresa el rango superior de la sumatoria:"))
+
+    media = discrete_media(i, n)
+    variance = discrete_variance(i, n, media)
+    deviation = standard_deviation(variance)
+
+    print('Media: {0} Varianza: {1} Desviación: {2}'.format(
+        media,
+        variance,
+        deviation
+        ))
+
+    input("Presiona [Enter] para continuar...")
+
+def discrete_media(i, n):
+
+    media = 0
+
+    while (i < n):
+        media += (i/n)
+        i += 1
+
+    return media
+
+def discrete_variance(i, n, media):
+
+    variance = 0
+
+    while(i <= n):
+        sub = i - media
+        variance += (math.pow(sub, 2) / n)
+        i += 1
+
+    return variance
+
+
+def standard_deviation(variance):
+    return math.sqrt(variance)
+
 
 def binomial_distribution():
-    print
+    print("Dist")
+
 def poisson_distribution():
     print("Dist")
     input("Press [Enter] to continue...")
@@ -20,7 +61,7 @@ functions = [
         {   "name": "Distrubución de probabilidad discreta general",
             "function": distribution },
         {   "name": "Distribución Binomial",
-            "function": binomial_distribition },
+            "function": binomial_distribution },
         {   "name": "Distribución de Poisson",
             "function": poisson_distribution },
         { "name": "Exit", "function": exit },
